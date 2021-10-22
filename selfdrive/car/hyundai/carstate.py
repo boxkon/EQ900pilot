e25ace2b6f5a5fc22b9ec2fb21671153ada18cb5
boxkon <70835190+boxkon@users.noreply.github.com>
2021-10-22 16:28:19 +0900

    cleanup
    cleanup

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -146,7 +146,7 @@ class CarState(CarStateBase):
       ret.gas = cp.vl["EMS12"]["PV_AV_CAN"] / 100.
       ret.gasPressed = bool(cp.vl["EMS16"]["CF_Ems_AclAct"])
 
-    if not self.car_fingerprint in FEATURES["use_elect_gears"]: # for display current state of gear
+    if not self.car_fingerprint in FEATURES["use_elect_gears"]: # for display current state of gear by Tenesi
       ret.currentGear = cp.vl["LVR11"]["CF_Lvr_CGear"]
 
 
@@ -216,7 +216,7 @@ class CarState(CarStateBase):
     # for activate HDA
     if self.CP.carFingerprint in FEATURES["send_hda_state_2"]:
       self.lfahda = copy.copy(cp_cam.vl["LFAHDA_MFC"])
-	  
+
     return ret
 
   @staticmethod
@@ -267,16 +267,16 @@ class CarState(CarStateBase):
 
       ("ESC_Off_Step", "TCS15", 0),
 
-      ("Lvr12_00", "LVR12", 0),     # 테네시 추가
-      ("Lvr12_01", "LVR12", 0),     # 테네시 추가
-      ("Lvr12_02", "LVR12", 0),     # 테네시 추가
-      ("Lvr12_03", "LVR12", 0),     # 테네시 추가
-      ("Lvr12_04", "LVR12", 0),     # 테네시 추가
-      ("Lvr12_05", "LVR12", 0),     # 테네시 추가
-      ("Lvr12_06", "LVR12", 0),     # 테네시 추가
-      ("Lvr12_07", "LVR12", 0),     # 테네시 추가
+      ("Lvr12_00", "LVR12", 0),      # for display current state of gear by Tenesi
+      ("Lvr12_01", "LVR12", 0),
+      ("Lvr12_02", "LVR12", 0),
+      ("Lvr12_03", "LVR12", 0),
+      ("Lvr12_04", "LVR12", 0),
+      ("Lvr12_05", "LVR12", 0),
+      ("Lvr12_06", "LVR12", 0),
+      ("Lvr12_07", "LVR12", 0),
 
-      ("CF_Lvr_CGear", "LVR11", 0), # 테네시 추가
+      ("CF_Lvr_CGear", "LVR11", 0),  # for display current state of gear by Tenesi
       ("CF_Lvr_GearInf", "LVR11", 0),  # Transmission Gear (0 = N or P, 1-8 = Fwd, 14 = Rev)
 
 
