c5cb735eacb17b1290b00173a35641d6ed5b0560
neokii <neokii@gmail.com>
2022-05-24 13:04:07 +0900

    merge changes

--- selfdrive/modeld/thneed/thneed.cc
+++ selfdrive/modeld/thneed/thneed.cc
@@ -9,10 +9,8 @@
 #include <map>
 #include <string>
 
-#include "selfdrive/common/clutil.h"
-#include "selfdrive/common/timing.h"
-//#define RUN_DISASSEMBLER
-#define RUN_OPTIMIZER
+#include "common/clutil.h"
+#include "common/timing.h"
 
 Thneed *g_thneed = NULL;
 int g_fd = -1;
